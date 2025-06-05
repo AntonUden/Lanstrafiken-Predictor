f7c7278fbece849bd2a8f775bbb77c6e806de324
Anton Udén <udenanton@hotmail.com>
2025-06-05 17:39:04 +0200

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,34 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.scss'
 })
-export class App {
-  protected title = 'lanstrafiken';
+export class App implements OnInit, OnDestroy {
+  readonly reasons = ["Signalfel", "Växelfel", "Obehöriga på spåret", "Spårarbete", "Personalbrist", "Dörrfel", "Tågförsening"];
+  stopped = false;
+  index = 0;
+  interval?: any;
+
+  get reason() {
+    return this.reasons[this.index];
+  }
+
+  next() {
+    this.index = (this.index + 1) % this.reasons.length;
+  }
+
+  toggle() {
+    this.stopped = !this.stopped;
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
+
+  ngOnInit(): void {
+    this.interval = setInterval(() => {
+      if (!this.stopped) {
+        this.next();
+      }
+    }, 100);
+  }
+
 }
